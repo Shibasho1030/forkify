f51b3e0bd1e849a5b8de380621a2fc557a0b958e
Shibasho1030 <shibasho10300@gmail.com>
2026-04-29 12:06:19 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '15f65237-54b4-43ea-a75d-fc25a90469cd';
+export const MODAL_CLOSE_SEC = 2.5;
